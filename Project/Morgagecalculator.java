b58b23099f5665bc948ea80553caa7c98212da36
unknown <ahhurayra2004@gmail.com>
2025-11-07 01:19:13 +0600

    25-61926-2

--- Project/Morgagecalculator.java
+++ Project/Morgagecalculator.java
@@ -1,27 +1,62 @@
 import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Morgagecalculator {
     public static void main(String[] args){
+        Locale bdlocale = Locale.of("en","BD");
         System.out.println("==Mortgage Calculator==");
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Loan Amount: ");
-        int p = input.nextInt();
-        System.out.println("Enter Annual Interest Rate: ");
-        float r = input.nextFloat();
-        System.out.println("Enter Loan Period: " );
-        int n = input.nextInt();
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter Salary: ");
+        double salary = sc.nextDouble();
+         if (salary>=40000) {
+            System.out.println("Next");
+        }
+        else{System.out.println("You are not elegible for loan.");}
+        
+        
+        System.out.print("Enter your cridet score: ");
+        double credit = sc.nextDouble();
+        if(credit>=300 || credit==500){
+            System.out.println("Next");
+        }
+        else{System.out.println("You are not elegible for loan.");}
 
 
-        double M = p*(r*Math.pow((1+r),n))/(Math.pow( (1+r) , n)-1);
-        double Monthly = M /(n*12);
+        System.out.println("Criminal record: ");
+        boolean record = sc.nextBoolean();
+        if(record == true || record != false ){
+            System.out.println("You are not elegible for loan.");
+        }
+         else {
+        System.out.println("Next");
+    }
+
+
+
+
+
+        
+        System.out.print("Enter Loan Amount(BDT): ");
+        int p = sc.nextInt();
+        System.out.print("Enter Annual Interest Rate: ");
+        float R = sc.nextFloat();
+        System.out.print("Enter Loan Period: " );
+        int n = sc.nextInt();
+
+        double r = R/100;
+        double M =p+ p*(r*Math.pow((1+(r)),n))/(Math.pow( (1+(r)) , n)-1);
+        double Monthly = (M) /(n*12);
         double I = M-p;
+         
 
 
 
 
-
-        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(bdlocale);
+        
         String formattedValue = currencyFormatter.format(p);
         System.out.println("Loan Amount: "+formattedValue);
 
@@ -38,6 +73,9 @@ public class Morgagecalculator {
 
         String formattedInterest = currencyFormatter.format(I);
         System.out.println("Total Interest Amount: "+formattedInterest);
+
+        String formattedTotalamount = currencyFormatter.format(M);
+        System.out.println("Total Amount: "+formattedTotalamount);
 
 
 
